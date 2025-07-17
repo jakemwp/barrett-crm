a764bfc13218a2a9861cfd776eaea4e9b35b0b99
jakemwp <jakebiehl@gmail.com>
2025-07-17 13:28:19 -0500

    Fix AuthProvider context issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
 import { ProtectedRoute } from './components/auth/ProtectedRoute';
 import { MainLayout } from './components/layout/MainLayout';
 import { Login } from './pages/Login';
@@ -22,45 +21,43 @@ import { CustomerLogin } from './pages/CustomerLogin';
 
 function App() {
   return (
-    <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/customer-login" element={<CustomerLogin />} />
-        <Route path="/" element={
-          <ProtectedRoute>
-            <MainLayout />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/customer-login" element={<CustomerLogin />} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <MainLayout />
+        </ProtectedRoute>
+      }>
+        <Route index element={<Dashboard />} />
+        <Route path="customer-portal" element={
+          <ProtectedRoute requiredRole="Customer">
+            <CustomerPortal />
           </ProtectedRoute>
-        }>
-          <Route index element={<Dashboard />} />
-          <Route path="customer-portal" element={
-            <ProtectedRoute requiredRole="Customer">
-              <CustomerPortal />
-            </ProtectedRoute>
-          } />
-          <Route path="clients" element={<Customers />} />
-          <Route path="clients/new" element={<AddCustomer />} />
-          <Route path="clients/:id" element={<CustomerDetail />} />
-          {/* Legacy routes for backward compatibility */}
-          <Route path="customers" element={<Navigate to="/clients" replace />} />
-          <Route path="customers/new" element={<Navigate to="/clients/new" replace />} />
-          <Route path="customers/:id" element={<CustomerDetail />} />
-          <Route path="vehicles" element={<Vehicles />} />
-          <Route path="vehicles/new" element={<AddVehicle />} />
-          <Route path="vehicles/:id" element={<VehicleDetail />} />
-          <Route path="check-in-out" element={<CheckInOutList />} />
-          <Route path="check-in-out/new" element={<AddCheckInOut />} />
-          <Route path="check-in-out/:id" element={<CheckInOutDetail />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="help" element={<Help />} />
-          <Route path="users" element={
-            <ProtectedRoute requiredRole="Admin">
-              <UserManagement />
-            </ProtectedRoute>
-          } />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </AuthProvider>
+        } />
+        <Route path="clients" element={<Customers />} />
+        <Route path="clients/new" element={<AddCustomer />} />
+        <Route path="clients/:id" element={<CustomerDetail />} />
+        {/* Legacy routes for backward compatibility */}
+        <Route path="customers" element={<Navigate to="/clients" replace />} />
+        <Route path="customers/new" element={<Navigate to="/clients/new" replace />} />
+        <Route path="customers/:id" element={<CustomerDetail />} />
+        <Route path="vehicles" element={<Vehicles />} />
+        <Route path="vehicles/new" element={<AddVehicle />} />
+        <Route path="vehicles/:id" element={<VehicleDetail />} />
+        <Route path="check-in-out" element={<CheckInOutList />} />
+        <Route path="check-in-out/new" element={<AddCheckInOut />} />
+        <Route path="check-in-out/:id" element={<CheckInOutDetail />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="help" element={<Help />} />
+        <Route path="users" element={
+          <ProtectedRoute requiredRole="Admin">
+            <UserManagement />
+          </ProtectedRoute>
+        } />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
   );
 }
 
