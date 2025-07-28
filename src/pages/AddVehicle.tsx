c6c350fe2d3f1a5a4303d66949c578883f0d2655
jakemwp <jakebiehl@gmail.com>
2025-07-28 10:41:45 -0500

    Updated AddVehicle.tsx

--- src/pages/AddVehicle.tsx
+++ src/pages/AddVehicle.tsx
@@ -562,7 +562,6 @@ export function AddVehicle() {
               </CardTitle>
             </CardHeader>
             <CardContent>
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <Input
                   label="State *"
@@ -968,7 +967,6 @@ export function AddVehicle() {
               </div>
             </CardContent>
           </Card>
-
         </div>
       </div>
     </div>
